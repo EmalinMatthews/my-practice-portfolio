3e95fe69b8b6ae6a9d6573b2895d3a1632f06231
EmalinMatthews <emalin.matthews@coop.co.uk>
2024-10-07 12:54:57 +0100

    practice portfolio project setup

---
+++ src/components/About.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import AboutImg from "../assets/about-img.png";
+
+const About = () => {
+  return (
+    <section className="bg-secondery text-white px-5 py-32" id="about">
+      <div className="container mx-auto grid md:grid-cols-2 items-center justify-center md:justify-between">
+        <div className="about-info">
+          <h2 className="text-4xl font-bold mb-5 border-b-[5px] w-[180px] border-indigo-600 pb-2">
+            About Me
+          </h2>
+
+          <p className="pb-5">
+            Hi, My Name Is Emalin Matthews I still need to fill this in
+          </p>
+          <p className="pb-5">
+           Gonna add more stuff here to blah blah blah blah
+           blah blah blah blah blah
+          </p>
+
+          <p>Little bit more here aswell</p>
+
+          <p>
+          An then add a little more here too
+          </p>
+        </div>
+
+        <div className="about-img">
+          <img
+            src={AboutImg}
+            alt="coding illustration"
+            className="lgw-[80%] md:ml-auto"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
