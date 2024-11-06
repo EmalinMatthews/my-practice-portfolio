21ecb05f5220a41c7b8cba977bad07a83e8542d3
EmalinMatthews <emalin.matthews@coop.co.uk>
2024-11-06 16:52:18 +0000

    Navlink added to mobile nav and removed onclick - code still needs work

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import DateTime from "./DateTime";
 import NavBar from "./NavBar";
+import { NavLink } from "react-router-dom";
 
 
 const Header = () => {
@@ -12,7 +13,7 @@ const Header = () => {
   return (
     <header className="flex justify-between px-5 py-2 bg-primary text-white fixed w-full z-10">
       <a href="/" className="logo text-2xl font-bold text-accent">
-        Emalin
+        Em
       </a>
 
       <NavBar/>
@@ -23,21 +24,21 @@ const Header = () => {
         className={!toggle ? "mobile-nav left-[-100%]" : "mobile-nav left-0"}
       >
         <ul className="flex flex-col">
-          <li>
-            <a href="/#about" onClick={handleToggle}>About</a>
-          </li>
-          <li>
-            <a href="/#projects" onClick={handleToggle}>Projects</a>
-          </li>
-          <li>
-            <a href="/#blog" onClick={handleToggle}>Blog</a>
-          </li>
-          <li>
-            <a href="/#contact" onClick={handleToggle}>Contact</a>
-          </li>
-          <li>
-            <a href="/#top" onClick={handleToggle}>Home</a>
-          </li>
+        <li>
+          <NavLink to="/#HomePage">HomePage</NavLink>
+        </li>
+        <li>
+          <NavLink to="/AboutPage">About</NavLink>
+        </li>
+        <li>
+          <NavLink to="/ProjectsPage">Projects</NavLink>
+        </li>
+        <li>
+          <NavLink to="/BlogPage">Blog</NavLink>
+        </li>
+        <li>
+          <NavLink to="/ContactPage">Contacts</NavLink>
+        </li> 
         </ul>
       </nav>
 
