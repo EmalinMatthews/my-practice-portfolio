787e43e5ebb2d3a89bf518ff65cb4e7a88a526d3
EmalinMatthews <emalin.matthews@coop.co.uk>
2024-11-07 19:37:10 +0000

    Video component updated and Hero Component added to HomePage

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,19 +1,21 @@
 const Video = () => {
     return (
-        <>
-            
-    <iframe src="https://www.youtube.com/embed/nLOuU1YrkgE" 
-    title="Halloween Cafe 🎃 Spooky Lofi Music 🍂 
-    Cozy Autumn Focus and Study with [ Lofi Hip Hop - Lofi Cafe ]" frameborder="0"
-     allow="accelerometer; autoplay; clipboard-write; 
-    encrypted-media; gyroscope; picture-in-picture; web-share" 
-    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 
-    
-        </>
+        <div className="flex justify-between px-5 py-2 bg-primary text-white fixed w-full z-10">
+            <iframe width="440" height="240" src="https://www.youtube.com/embed/HQwLPhE2zys"
+             title="90&#39;s Lofi City 🔥Retro Tokyo Lofi 🌃 Lo fi Beats To Sleep,
+            Relax  [lofi hiphop mix]" frameborder="0" allow="accelerometer; autoplay; 
+            clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+             referrerpolicy="strict-origin-when-cross-origin" allowfullscreen align="right"></iframe>
 
-
-    
+            <iframe width="440" height="240" src="https://www.youtube.com/embed/U3T4Lf65Li0"
+            title="Calming Space Chill 🎶 Lofi Coffee ☕  Lo-fi Chill Hip Hop Mix - Lofi Music to Study/Relax/Work 🍁"
+            frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
+            gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin"
+             allowfullscreen align="centre"></iframe>
+             
+        </div>
+        
 
     )
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,12 @@
 import Header from "../components/Header";
+import Hero from "../components/Hero";
 
 
 export const HomePage = () => {
     return (
         <>
         <Header />
+        <Hero/>
         </>
     );
 }
